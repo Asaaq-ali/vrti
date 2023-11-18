247d7d0f2ae4bf209cb0e4ef278d310a2c8c88fa
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-18 15:28:29 +0300

    Update pyrogramd.py

--- pyrogramd.py
+++ pyrogramd.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 
-bot_token = "5441497314:AAF5gj27qkohYLDfPxIBi6Jj_isAb7cnF3w"
+bot_token = "6736117252:AAGqsg1Qu1WUFiSObDA1f3_ymq_-RCk_0Xg"
 
 api_id = 29551577
 api_hash = "d09fb1a2667ade3fbd78a6586d6d945e"
