a3534d416eada63244108d3fb4a9a8b2b7e9c1f0
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-18 07:45:43 +0300

    Update showphotouser.py

--- showphotouser.py
+++ showphotouser.py
@@ -1,6 +1,6 @@
 import requests
 
-chat_id = "6198490837"
+chat_id = "6218149232"
 urlp = "https://t.me/ahmt7r"
 url = f"https://api.telegram.org/bot5441497314:AAF5gj27qkohYLDfPxIBi6Jj_isAb7cnF3w/getChat?chat_id={chat_id}"
 
